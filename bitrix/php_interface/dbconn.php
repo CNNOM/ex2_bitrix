59b235d2211b5794b8a161aca89683a3558c82d4
Zenkov <zenkov.m700@mail.ru>
2025-06-03 10:39:05 +0300

    ex2-610

---
+++ bitrix/php_interface/dbconn.php
@@ -0,0 +1,14 @@
+<?php
+$DBDebug = false;
+$DBDebugToFile = false;
+
+define("BX_FILE_PERMISSIONS", 0644);
+define("BX_DIR_PERMISSIONS", 0755);
+@umask(~(BX_FILE_PERMISSIONS | BX_DIR_PERMISSIONS) & 0777);
+
+define("BX_DISABLE_INDEX_PAGE", true);
+
+mb_internal_encoding("UTF-8");
+
+define('BX_CRONTAB_SUPPORT', false);
+define('BX_CHECK_AGENT_START', false); 
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -6,5 +6,6 @@ if (file_exists($_SERVER["DOCUMENT_ROOT"] . '/local/php_interface/event.php')) {
 if (file_exists($_SERVER["DOCUMENT_ROOT"] . '/local/php_interface/const.php')) {
     require_once($_SERVER["DOCUMENT_ROOT"] . '/local/php_interface/const.php');
 }
-
-
+if (file_exists($_SERVER["DOCUMENT_ROOT"] . '/local/php_interface/agents.php')) {
+    require_once($_SERVER["DOCUMENT_ROOT"] . '/local/php_interface/agents.php');
+}
